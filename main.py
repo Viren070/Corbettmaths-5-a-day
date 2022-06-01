be3ff57ebb549a9b79a7a01a6405895a0772b227
Viren Rajeskumar <71220264+Viren070@users.noreply.github.com>
2022-06-01 12:23:54 +0000

    -program doesn't close after one try. -fixed answers for march due to it having a slightly different link. -more functions

--- main.py
+++ main.py
@@ -5,15 +5,36 @@ def show_text(title,text):
     messagebox.showerror(title, text)
 def close():
     if clicked.get()!="Select month" and clicked2.get!="Select day" and clicked3.get!="Select type":
-        root.destroy()
+        return True
     else:
         show_text('Error','You must select a type, day and month!')
+        return False
+def task():
+    close()
+    if close:
+        month=clicked.get()
+        day=clicked2.get()
+        Type=clicked3.get()
+
+        if Type=="Answers":
+            if month=="March":
+                link='https://corbettmaths.com/wp-content/uploads/2021/09/HP-Ans-'+month+'-'+day+'.pdf'
+            else:    
+                link='https://corbettmaths.com/wp-content/uploads/2021/08/HP-Ans-'+month+'-'+day+'.pdf'
+            webbrowser.open(link, new=2)
+            print("In case a new tab did not open, here is the link: \n"+link)
+
+          
+        elif Type=="Questions":
+          link='https://corbettmaths.com/wp-content/uploads/2021/08/Higher-Plus-'+month+'_Part'+day+'.pdf'
+          webbrowser.open(link, new=2)
+          print("In case a new tab did not open, here is the link: \n"+link)
 
         
 
 root = Tk()
 root.title("Higher Plus 5-a-day")
-root.geometry( "190x240" )
+root.geometry( "400x200" )
 options = [
 	"Jan",
 	"Feb",
@@ -42,36 +63,18 @@ clicked2.set( "Select day" )
 clicked3.set( "Select type" )
 
 drop = OptionMenu( root , clicked3 , *options3 )
-drop.pack()
+drop.place(x=25,y=1)
 drop2 = OptionMenu( root, clicked2, *options2 )
-drop2.pack()
+drop2.place(x=140,y=1)
 drop3= OptionMenu ( root, clicked, *options )
-drop3.pack()
-
-
-Button(
-    root, 
-    text="Open PDF", 
-    font=("Times", 13),
-    command=close
-    ).pack()
-
+drop3.place(x=255,y=1)
+Button(root, text="Open PDF", font=("Times", 13), command=task).place(x=156,y=50)
+drop.config(width=12)
+drop2.config(width=12)
+drop3.config(width=12)
 root.mainloop()
 
-month=clicked.get()
-day=clicked2.get()
-Type=clicked3.get()
 
-if Type=="Answers":
-  link='https://corbettmaths.com/wp-content/uploads/2021/08/HP-Ans-'+month+'-'+day+'.pdf'
-  webbrowser.open(link, new=2)
-
-  
-elif Type=="Questions":
-  link='https://corbettmaths.com/wp-content/uploads/2021/08/Higher-Plus-'+month+'_Part'+day+'.pdf'
-  webbrowser.open(link, new=2)
-
-print("In case a new tab did not open, here is the link: \n"+link)
     
     
 
