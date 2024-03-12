0f07f1959abcc938d3fe82b1bc8006559ace35dc
Viren070 <viren070@protonmail.com>
2024-03-12 17:23:36 +0000

    fix: correctly identify higher plus and foundation plus links

--- generate_dict.py
+++ generate_dict.py
@@ -6,7 +6,7 @@ import json
 
 
 
-def create_dict():
+def create_dict(log=False):
     questions_page_url = "https://corbettmaths.com/5-a-day/gcse/"
     page = requests.get(questions_page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -15,10 +15,11 @@ def create_dict():
     difficulty_levels = {
         "Numeracy": ["Numeracy"],
         "Foundation": ["Foundation"],
-        "Foundation Plus": ["Foundation Plus", "FP"],
+        "Foundation Plus": ["Foundation Plus", "FP", "Foundation-Plus"],
         "Higher": ["Higher"],
-        "Higher Plus": ["Higher Plus", "HP"]
+        "Higher Plus": ["Higher-Plus", "HP", "Higher Plus"]
     }
+    sorted_difficulty_levels = sorted(difficulty_levels.items(), key=lambda x: -len(x[0]))
     page_dict = {}
     for link in links:
         url = link.get('href')
@@ -27,10 +28,11 @@ def create_dict():
 
         filename = url.split('/')[-1]
         identified_month = next((month for month in months if month.lower() in filename.lower()), None)
-        identified_difficulty = next((level for level, keywords in difficulty_levels.items() for keyword in keywords if keyword.lower() in filename.lower()), None)
+        identified_difficulty = next((level for level, keywords in sorted_difficulty_levels for keyword in keywords if keyword.lower() in filename.lower()), None)
         numbers = re.findall(r'\d+', filename)
         identified_day = numbers[0] if numbers else None
-
+        if not identified_difficulty:
+            identified_difficulty = link.text
         if identified_month and identified_difficulty:
             page_dict.setdefault(identified_month, {"Answers": {}, "Questions": {}})
             page_dict[identified_month]["Questions"].setdefault(identified_day, {})[identified_difficulty] = url
@@ -68,3 +70,6 @@ def create_dict():
 
     with open("corbettmaths.json", "w") as f:
         json.dump(page_dict, f)
+
+if __name__ == "__main__":
+    create_dict(log=True)
